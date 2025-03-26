0b8de6da9e668133edd2465464102a0547545c6e
LUser8 <atultiwarikec@gmail.com>
2025-03-26 15:26:31 +0530

    tokens creation

---
+++ 38_tokenization.py
@@ -0,0 +1,23 @@
+
+# import nltk
+# nltk.download('punkt_tab')
+
+from nltk.tokenize import sent_tokenize, word_tokenize, wordpunct_tokenize
+from nltk.
+
+corpus = """Welcome! Hello my name is Atul. 
+I am a software developer. You are watching atul's code.
+My specification in python language, I love to coding.
+"""
+
+### Tokenization - paragraph to sentenses 
+documents = sent_tokenize(corpus)
+print(documents) 
+
+### Tokenization - sentences to words
+words = word_tokenize(corpus)
+print(words)
+
+### Tokenization - sentences to words
+words = wordpunct_tokenize(corpus)
+print(words)
